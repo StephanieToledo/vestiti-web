d4989a9959ed1bee97c98a2850ba6b78530efe5b
StephanieToledo <stephanie.lt1@hotmail.com>
2021-05-21 12:32:16 -0300

    feat; finishing cards of home

--- src/components/HomeCard/index.js
+++ src/components/HomeCard/index.js
@@ -5,6 +5,12 @@ import {
 from './style';
 import Jeans from '../../assets/images/home-6.png';
 import IconFavoriteNove from '../../assets/images/favorite-none.png';
+import Cart from '../../assets/images/cart-card.png';
+import Dress from '../../assets/images/home-3.jpg';
+import Clote from '../../assets/images/home-7.jpg';
+import Shirt from '../../assets/images/home-8.jpg';
+import Overalls from '../../assets/images/home-9.jpg';
+import Skirt from '../../assets/images/home-10.jpg';
 
 function HomeCard() {
 	return (
@@ -13,6 +19,60 @@ function HomeCard() {
 				<Image>
 					<img className="favorite" src={IconFavoriteNove} alt="Favoritar" />
 					<img className="image" src={Jeans} alt="Calça Jeans" />
+					<div className="container-price-cart">
+						<h3>R$ 200,00</h3>
+						<img className="cart" src={Cart} alt="Carrinho" />
+					</div>
+				</Image>
+			</Card>
+			<Card>
+				<Image>
+					<img className="favorite" src={IconFavoriteNove} alt="Favoritar" />
+					<img className="image" src={Dress} alt="Vestido" />
+					<div className="container-price-cart">
+						<h3>R$ 100,00</h3>
+						<img className="cart" src={Cart} alt="Carrinho" />
+					</div>
+				</Image>
+			</Card>
+			<Card>
+				<Image>
+					<img className="favorite" src={IconFavoriteNove} alt="Favoritar" />
+					<img className="image" src={Clote} alt="Blusa" />
+					<div className="container-price-cart">
+						<h3>R$ 270,00</h3>
+						<img className="cart" src={Cart} alt="Carrinho" />
+					</div>
+				</Image>
+			</Card>
+			<Card>
+				<Image>
+					<img className="favorite" src={IconFavoriteNove} alt="Favoritar" />
+					<img className="image" src={Overalls} alt="Macacão" />
+					<div className="container-price-cart">
+						<h3>R$ 270,00</h3>
+						<img className="cart" src={Cart} alt="Carrinho" />
+					</div>
+				</Image>
+			</Card>
+			<Card>
+				<Image>
+					<img className="favorite" src={IconFavoriteNove} alt="Favoritar" />
+					<img className="image" src={Shirt} alt="Camisa" />
+					<div className="container-price-cart">
+						<h3>R$ 70,00</h3>
+						<img className="cart" src={Cart} alt="Carrinho" />
+					</div>
+				</Image>
+			</Card>
+			<Card>
+				<Image>
+					<img className="favorite" src={IconFavoriteNove} alt="Favoritar" />
+					<img className="image" src={Skirt} alt="Saia" />
+					<div className="container-price-cart">
+						<h3>R$ 65,00</h3>
+						<img className="cart" src={Cart} alt="Carrinho" />
+					</div>
 				</Image>
 			</Card>
 		</Container>
--- src/components/HomeCard/style.js
+++ src/components/HomeCard/style.js
@@ -1,21 +1,26 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-	width: 95%;
-	height: 500px;
-	margin-bottom: 1em;
+	max-width: 95%;
+	height: auto;
+	margin-bottom: 2em;
 	display: flex;
 	align-items: center;
 	justify-content: space-around;
+	flex-wrap: wrap;
 	margin-top: 1em;
 	margin-left: 2.5%;
-	background-color: #F3F3F5;
+	background-color: #FCFCFC;
 `;
 
 export const Card = styled.div`
-	width: 20em;
+	width: 17em;
 	height: 450px;
 	border: 1px solid;
+	border-color: #BFBFBF;
+	margin-top: 1em;
+	margin-bottom: 1em;
+	margin: 0.6em;
 `;
 
 export const Image = styled.div`
@@ -24,14 +29,52 @@ export const Image = styled.div`
 
 	.image {
 		width: 15em;
-		height: 280px;
+		height: 300px;
+		padding-top: 0.7em;
+		margin-left: 1rem;
+		cursor: pointer;
+	}
+
+	.image:hover {
+		transition: 1s;
+		-webkit-transform: scale(1.1);
+		-moz-transform: scale(1.1);
+		-o-transform: scale(1.1);
+		-ms-transform: scale(1.1);
+		transform: scale(1.1);
 	}
 
 	.favorite {
-		width: 30px;
-		height: 30px;
+		width: 8%;
+		height: 25px;
 		float: right;
-		margin-right: 1em;
-		margin-top: 1em;
+		margin-right: 0.6em;
+		margin-top: 0.4em;
+		cursor: pointer;
+	}
+
+	.container-price-cart {
+		width: 100%;
+		height: 60px;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	h3 {
+		width: 100px;
+		max-width: auto;
+		height: 30px;
+		margin: auto;
+		margin-left: 5.1em;
+		margin-top: 2em;
+		color: #4D2059;
+	}
+
+	.cart {
+		width: 2em;
+		height: 30px;
+		margin-right: 10px;
+		margin-top: 3.5em;
+		cursor: pointer;
 	}
 `;
